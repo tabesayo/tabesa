518ef6ed3a8e42b4c7c6efc82c98583695c9f748
tabesayo <109439173+tabesayo@users.noreply.github.com>
2022-10-31 22:27:05 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,5 +5,15 @@ const correct = 's';
 document.getElementById('js-question').textContent = question;
 
 document.getElementByTagName('button')[0].textContent = answers[0];
+document.getElementsByTagName('button')[1].textContent = answers[1];
+document.getElementsByTagName('button')[2].textContent = answers[2];
+document.getElementsByTagName('button')[3].textContent = answers[3];
 
+document.getElementsByTagName('button')[0].addEventListener('click', () => {
+  if(correct === document.getElementsByTagName('button')[0].textContent){
+    window.alert('正解');
+  }else{
+    window.alert('不正解');
+  }
+});
 
