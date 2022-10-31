ae33112e433121ee7801232e0a35e8ff389d58c2
tabesayo <109439173+tabesayo@users.noreply.github.com>
2022-10-31 22:45:56 +0900

    Update app.js

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const answers = [
 ];
 const correct = 's';
 
-do.getElementById('js-question').textContent = question;
+document.getElementById('js-question').textContent = question;
 
-do.getElementsByTagName('button')[0].textContent = answers[0];
+document.getElementsByTagName('button')[0].textContent = answers[0];
 document.getElementsByTagName('button')[1].textContent = answers[1];
 document.getElementsByTagName('button')[2].textContent = answers[2];
 document.getElementsByTagName('button')[3].textContent = answers[3];
