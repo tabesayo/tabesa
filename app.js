cea3931dd0854422fc7ef8232297e6cc652400f1
tabesayo <109439173+tabesayo@users.noreply.github.com>
2022-10-31 22:05:37 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const question = 'ゲーム、市場';
 const answers = ['あ','s','sa','sh'];
 const correct = 's';
 
-document.getElementById('js-question').textContent ='question';
+document.getElementById('js-question').textContent = question;
 
 
