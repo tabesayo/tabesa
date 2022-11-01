30cb65a909f6ea32ce6d05a6741ce5ea43e8e992
tabesayo <109439173+tabesayo@users.noreply.github.com>
2022-11-01 20:51:41 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,68 +1,75 @@
 const quiz = [
   { 
     question: '初めて行ったフェスはなんでしょう？',
-    answers: [
-  'ロッキン','CDJ','ビバラ','ラブシャ'
-],
- correct: 'ビバラ'
-}, {
-  question: '好きなライブ会場は？',
-    answers: [
-  'ZeppDiverCity','新木場STUDIOCOAST','O-East','豊洲PIT'
-      ],
-  correct: '新木場STUDIOCOAST'
-}, {
+    answers: ['ロッキン','CDJ','ビバラ','ラブシャ'],
+    correct: 'ビバラ'
+   }, {
+    question: '好きなライブ会場は？',
+    answers: ['ZeppDiverCity','新木場STUDIOCOAST','O-East','豊洲PIT'],
+    correct: '新木場STUDIOCOAST'
+   }, {
       question: 'ライブ後と言えば？',
-    answers: [
-  '牛丼','焼肉','ハンバーグ','ラーメン'
-      ],
-  correct: 'ラーメン'
-}
+      answers: ['牛丼','焼肉','ハンバーグ','ラーメン'],
+     correct: 'ラーメン'
+   }
 ];
-const quizLength = quiz.length;
-let quizIndex = 0;
+
+const $window = window;
+const $doc = document;
+const $question = $doc.getElementById('js-question');
+const $buttons = $doc.querySelectorAll('.btn');
+
+const quizLen = quiz.length;
+let quizCount = 0;
 let score = 0;
 
-const $button = document.getElementByTagName('button');
-const buttonLength = $button.length;
-//クイズの問題
-const setupQuiz = () => {
-  document.getElementById('js-question').textContent = quiz[quizIndex].question;
-let buttonIndex = 0;
-while(buttonIndex < buttonLength){
-  $button[buttonIndex].textConetnt = quiz[quizIndex].answers[buttonIndex];
-  buttonIndex++;
-}
-}
-setupQuiz();
-const clickHandler = (e) => {
-  if(quiz[quizIndex].correct === e.target.textContent){
-    window.alert('正解');
+const init = () => {
+  $question.textContent = quiz[quizCount].question;
+  
+  const buttonLen = $buttons.length;
+  let btnIndex = 0;
+  
+  while(btnIndex < buttonLen){
+    $buttons[btnIndex].textContent = quiz[quizCount].answers[btnIndex];
+    btnIndex++;
+  }
+};
+
+const goToNext = () => {
+  quizCount++;
+  if(quizCount < quizLen){
+    init(quizCount);
+  } else {
+    // $window.alert('クイズ終了！');
+    showEnd();
+  }
+};
+
+const judge = (elm) => {
+  if(elm.textContent === quiz[quizCount].correct){
+    $window.alert('正解!');
     score++;
   } else {
-    window.alert('不正解');
+    $window.alert('不正解!');
   }
-   quizIndex++;
-  if(quizIndex < quizLength){
-    setupQuiz();
-    } else{
-      window.alert('終了!あなたの正解数は' + score + '/' + quizLength + 'です！');
-      }
-  };
+  goToNext();
+};
 
-let handlerIndex = 0;
-while (handlerIndex < buttonLength) {
-  handlerIndex++;
-  }
-$button[0].addEventListener('click', (e) => {
- clickHandler();
-});
-$button[1].addEventListener('click', (e) => {
-  clickHandler();
-});
-$button[2].addEventListener('click', (e) => {
-  clickHandler();
-});
-$button[3].addEventListener('click', (e) => {
-  clickHandler();
-});
+const showEnd = () => {
+  $question.textContent = '終了！あなたのスコアは' + score + '/' + quizLen + 'です';
+  
+  const $items = $doc.getElementById('js-items');
+  $items.style.visibility = 'hidden';
+};
+
+init();
+
+let answersIndex = 0;
+let answersLen = quiz[quizCount].answers.length;
+
+while(answersIndex < answersLen){
+  $buttons[answersIndex].addEventListener('click', (e) => {
+    judge(e.target);
+  });
+  answersIndex++;
+}
